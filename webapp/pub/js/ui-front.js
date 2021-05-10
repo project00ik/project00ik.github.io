2a1f54c354ed6ed8a8b18c2d622b21cca614aca0
rahyunjin <ralla89@gmail.com>
2021-05-10 17:27:25 +0900

    [진행중] 데이터피커

--- webapp/pub/js/ui-front.js
+++ webapp/pub/js/ui-front.js
@@ -49,6 +49,20 @@ $(document).ready(function (){
 
   // });
 
+  
+  $( "#datepicker" ).datepicker({
+    dateFormat: 'yy-mm-dd',
+    prevText: '이전 달',
+    nextText: '다음 달',
+    monthNames: ['01','02','03','04','05','06','07','08','09','10','11','12'],
+    monthNamesShort: ['01','02','03','04','05','06','07','08','09','10','11','12'],
+    dayNames: ['일','월','화','수','목','금','토'],
+    dayNamesShort: ['일','월','화','수','목','금','토'],
+    dayNamesMin: ['일','월','화','수','목','금','토'],
+    showMonthAfterYear: true,
+    yearSuffix: '.'    
+  });
+
 });
 
 function allCategoryBtn(){
@@ -483,6 +497,7 @@ function scrollTab() {
     });
   });
 }
+
 
 
 // function btnTooltip(){
