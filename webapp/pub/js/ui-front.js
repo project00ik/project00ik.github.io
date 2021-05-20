c85ec322306abda3315c92b2281dacf1984e26a2
inkyung <inkyung0002@gmail.com>
2021-05-20 16:58:26 +0900

    [수정] datepicker 다중 사용으로 변경

--- webapp/pub/js/ui-front.js
+++ webapp/pub/js/ui-front.js
@@ -49,19 +49,20 @@ $(document).ready(function (){
 
   // });
 
-  
-  $( "#datepicker" ).datepicker({
-    dateFormat: 'yy-mm-dd',
-    prevText: '이전 달',
-    nextText: '다음 달',
-    monthNames: ['01','02','03','04','05','06','07','08','09','10','11','12'],
-    monthNamesShort: ['01','02','03','04','05','06','07','08','09','10','11','12'],
-    dayNames: ['일','월','화','수','목','금','토'],
-    dayNamesShort: ['일','월','화','수','목','금','토'],
-    dayNamesMin: ['일','월','화','수','목','금','토'],
-    showMonthAfterYear: true,
-    yearSuffix: '.'    
-  });
+  $(".datepicker").each(function(){
+    $(this).datepicker({
+      dateFormat: 'yy-mm-dd',
+      prevText: '이전 달',
+      nextText: '다음 달',
+      monthNames: ['01','02','03','04','05','06','07','08','09','10','11','12'],
+      monthNamesShort: ['01','02','03','04','05','06','07','08','09','10','11','12'],
+      dayNames: ['일','월','화','수','목','금','토'],
+      dayNamesShort: ['일','월','화','수','목','금','토'],
+      dayNamesMin: ['일','월','화','수','목','금','토'],
+      showMonthAfterYear: true,
+      yearSuffix: '.'    
+    });
+  })
 
 });
 
